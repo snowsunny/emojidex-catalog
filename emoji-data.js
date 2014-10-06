1b9ad6d9bbaed5351153c26c1fa62095e54b6bf2
snowsunny <snowsunny1107@gmail.com>
2014-10-06 15:32:28 +0900

    init & load sample

---
+++ emoji-data.js
@@ -0,0 +1,26 @@
+window.emojis_json = [
+  {
+    "moji": "🀄",
+    "full_name": "mahjong_tile_red_dragon",
+    "code": "mahjong",
+    "code_ja": "麻雀",
+    "category": "objects",
+    "unicode": "1f004"
+  },
+  {
+    "moji": "🃏",
+    "full_name": "playing_card_black_joker",
+    "code": "black_joker",
+    "code_ja": "ジョーカー",
+    "category": "objects",
+    "unicode": "1f0cf"
+  },
+  {
+    "moji": "🅰",
+    "full_name": "negative_squared_latin_capital_letter_A",
+    "code": "a",
+    "code_ja": "A型",
+    "category": "symbols",
+    "unicode": "1f170"
+  },
+]
